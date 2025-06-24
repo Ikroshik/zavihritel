ad99397700b09787ccd85a048ede26eceb39d37f
Dhoinich <khakimov7076@gmail.com>
2025-06-25 00:00:26 +0500

    Small fixes for slider and update for Timur

---
+++ app/public/wp-content/uploads/uag-plugin/assets/0/uag-js-9.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-03426337' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-68d7bcc7' );
+});
+window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-4e0391ad .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-4e0391ad .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-4e0391ad .swiper-button-next","prevEl":".uagb-block-4e0391ad .swiper-button-prev"}}	);
+});
+
+ });
